5cf2f35ff42cb3f5052080aa1bebc4f93857a8e3
Britton Perry <bper5656@740s-pl-132.local>
2016-09-28 08:48:41 -0600

    Added a date picker that actually works

---
+++ MyGUIPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
+
+let currectDate = NSDate()
+
+let date = UIDatePicker()
+date.datePickerMode = UIDatePickerMode.Date
+date.backgroundColor = UIColor.purpleColor()
+
+date
